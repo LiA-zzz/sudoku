504df785b189208230157e4a901a0b61fda4af33
Andy Li <liandy223@gmail.com>
2020-06-24 02:51:15 -0700

    Update to handle new board format

--- board.py
+++ board.py
@@ -1,14 +1,21 @@
 import os, random
+import solver
 
 class board:
     def __init__(self):
         self.board = []
-        #self.presetSquares = []
+        #self.presetSquares = [] 
+        #store as tuples squares which cannot be changed? or maybe reinforce this using the gui
         prefix = os.getcwd() + "\\boards\\"
         openFile = open(prefix+random.choice(os.listdir("boards")),'r')
         for line in openFile:
-            row = [int(val) for val in line.split() if val != '\n']
+            row = [val for val in line.split() if val != '\n']
             self.board.append(row)
+        openFile.close()
+        
+        self.solution = [row.copy() for row in self.board]
+        solver.solve(self.solution)
+        
     
     def __repr__(self):
         #temp console view of sudoku board.
@@ -25,7 +32,16 @@ class board:
         retStr+=vert
         return retStr
     
-    
+    def getSolution(self):
+        return self.solution
 
+    def getBoard(self):
+        return self.board
+    
+    def modifyBoard(self,val,row,col):
+        pass
+
+    def compareToAnswer(self,board):
+        pass
             
 
